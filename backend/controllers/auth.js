4f4abcba4572a977ac41b4e9e92a9aa654624311
LinC3e <ivancostilla17@gmail.com>
2023-06-22 14:02:04 -0300

    register user, adding to db

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,12 +1,30 @@
-const register = (req,res) => {
+const User = require("../models/User")
+
+const register = async (req, res) => {
     const { email, password, username } = req.body
 
-    console.log(email,password,username)
+    console.log(email, password, username)
 
-    res.send('Register')
+    try {
+        const newUser = new User({ username, email, password })
+        newUser.password = await newUser.passwordEncrypt(password)
+        const userSaved = await newUser.save()
+        
+        res.json({
+            id: userSaved._id,
+            email: userSaved.email,
+            username: userSaved.username,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
-const login = (req,res) => {
+const login = (req, res) => {
     res.send('Login')
 }
 
