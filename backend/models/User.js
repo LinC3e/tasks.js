26a77212e1916644145f31ae9107bcbed1507c83
LinC3e <ivancostilla17@gmail.com>
2023-06-22 13:59:33 -0300

    bcrypt - hashing pass

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, min: 3, max: 15, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
 }, { timestamps: true })
+
+userSchema.methods.passwordEncrypt = async (password) => {
+    const salt = await bcrypt.genSalt(10) // Semmilla para luego encryptar
+    return await bcrypt.hash(password, salt)
+}
+
+userSchema.methods.checkPassword = async function ( password ) { // true o false
+    return await bcrypt.compare(password, this.password)
+}
+
 
 module.exports = mongoose.model('User', userSchema)
