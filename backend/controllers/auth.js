8651f17a5a79267a64fa2bad282adf6c26985128
LinC3e <ivancostilla17@gmail.com>
2023-06-23 14:42:14 -0300

    login controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+
 const User = require("../models/User")
 const createAccessToken = require("../utils/jwt")
 
@@ -26,8 +28,31 @@ const register = async (req, res) => {
 
 }
 
-const login = (req, res) => {
-    res.send('Login')
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const userFound = await User.findOne({email})
+        if(!userFound) return res.status(400).json({ message: 'User not found.' })
+
+        const isMatch = await bcrypt.compare(password, userFound.password)
+
+        if(!isMatch) return res.status(400).json({ message: 'Invalid password.' })
+
+        const token = await createAccessToken({ id: userFound._id })
+
+        res.cookie('token', token)
+        res.json({
+            id: userFound._id,
+            email: userFound.email,
+            username: userFound.username,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 module.exports = {
