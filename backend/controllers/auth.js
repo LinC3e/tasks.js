fd4b2b34c2563b77fe86b5f47a25015243235c12
LinC3e <ivancostilla17@gmail.com>
2023-06-23 14:25:03 -0300

    jwt token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,15 +1,17 @@
 const User = require("../models/User")
+const createAccessToken = require("../utils/jwt")
 
 const register = async (req, res) => {
     const { email, password, username } = req.body
-
-    console.log(email, password, username)
 
     try {
         const newUser = new User({ username, email, password })
         newUser.password = await newUser.passwordEncrypt(password)
         const userSaved = await newUser.save()
-        
+
+        const token = await createAccessToken({ id: userSaved._id })
+
+        res.cookie('token', token)
         res.json({
             id: userSaved._id,
             email: userSaved.email,
@@ -19,7 +21,7 @@ const register = async (req, res) => {
         })
 
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ message: error.message })
     }
 
 }
