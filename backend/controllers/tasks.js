c5c79810dc37561e87050a1055bf056cd8f96b99
LinC3e <ivancostilla17@gmail.com>
2023-06-26 17:34:12 -0300

    ADD - Create task controller

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -10,6 +10,30 @@ const getTasks = async (req,res) => {
     }
 }
 
+// CREATE TASK
+const createTask = async (req,res) => {
+    try {
+        const { title , description , date } = req.body
+
+        const newTask = new Task({
+            title,
+            description,
+            date
+        })
+        const savedTask = newTask.save()
+        res.json({
+            title,
+            description,
+            date
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
     getTasks,
+    createTask
 }
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,11 +2,11 @@ const express = require('express')
 const tasksRoutes = express.Router()
 
 const isAuthenticated = require('../middlewares/isAuthenticated')
-const { getTasks } = require('../controllers/tasks')
+const { getTasks, createTask } = require('../controllers/tasks')
 
 tasksRoutes.get('/tasks', isAuthenticated, getTasks)
 tasksRoutes.get('/tasks/:id', isAuthenticated, )
-tasksRoutes.post('/tasks', isAuthenticated, )
+tasksRoutes.post('/tasks', isAuthenticated, createTask )
 tasksRoutes.delete('/tasks/:id', isAuthenticated, )
 tasksRoutes.put('/tasks/:id', isAuthenticated, )
 
