8ba8fd8f7a94b6bd5fd4cd3d8a1c3445685b99ac
LinC3e <ivancostilla17@gmail.com>
2023-06-26 16:46:04 -0300

    ADD - Tasks routes

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -4,6 +4,10 @@ const tasksRoutes = express.Router()
 const isAuthenticated = require('../middlewares/isAuthenticated')
 const { test } = require('../controllers/tasks')
 
-tasksRoutes.get('/tasks', isAuthenticated , test)
+tasksRoutes.get('/tasks', isAuthenticated, )
+tasksRoutes.get('/tasks/:id', isAuthenticated, )
+tasksRoutes.post('/tasks', isAuthenticated, )
+tasksRoutes.delete('/tasks/:id', isAuthenticated, )
+tasksRoutes.put('/tasks/:id', isAuthenticated, )
 
 module.exports = tasksRoutes
