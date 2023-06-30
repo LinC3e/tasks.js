c24383a5bd1d94e3972928553e7c06e050d92fd5
LinC3e <ivancostilla17@gmail.com>
2023-06-30 18:33:06 -0300

    ADD: Register Schema Validator with zod

---
+++ backend/validators/authSchema.js
@@ -0,0 +1,23 @@
+const z = require('zod')
+
+const registerSchema = z.object({
+    username: z.string({
+        required_error: "Username is required."
+    }).min(3, "Username must be more than 3 characters")
+    .max(15, "Username must be less than 15 characters.").trim(),
+    
+    email: z.string({
+        required_error: "Email is required"
+    }).email({
+        message: "Email invalid."
+    }).trim(),
+
+    password: z.string({
+        required_error: "Password is required.",
+    }).min(5, "Username must be more than 5 characters")
+
+})
+
+module.exports = {
+    registerSchema
+}
