fb53e0212f964d6dbc7a0067f92f305fca172566
LinC3e <ivancostilla17@gmail.com>
2023-06-30 18:42:32 -0300

    ADD - Login Schema Validator with zod

--- backend/validators/authSchema.js
+++ backend/validators/authSchema.js
@@ -4,8 +4,8 @@ const registerSchema = z.object({
     username: z.string({
         required_error: "Username is required."
     }).min(3, "Username must be more than 3 characters")
-    .max(15, "Username must be less than 15 characters.").trim(),
-    
+        .max(15, "Username must be less than 15 characters.").trim(),
+
     email: z.string({
         required_error: "Email is required"
     }).email({
@@ -18,6 +18,19 @@ const registerSchema = z.object({
 
 })
 
+const loginSchema = z.object({
+    email: z.string({
+        required_error: "Email is required"
+    }).email({
+        message: "Email invalid."
+    }).trim(),
+
+    password: z.string({
+        required_error: "Password is required.",
+    }).min(5, "Username must be more than 5 characters")
+})
+
 module.exports = {
-    registerSchema
+    registerSchema,
+    loginSchema
 }
