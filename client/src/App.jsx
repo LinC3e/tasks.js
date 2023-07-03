b84e9fb26cee161567180ddabfe86dbda3f25bb2
LinC3e <ivancostilla17@gmail.com>
2023-07-03 18:05:42 -0300

    ADD - Router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="text-sky-400/50 font-bold">Testing</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element= {<h1>Home Page</h1>} />
+        <Route path='/login' element= {<h1>Login Page</h1>} />
+        <Route path='/register' element= {<h1>Register Page</h1>} />
+        <Route path='/tasks' element= {<h1>Tasks Page</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
