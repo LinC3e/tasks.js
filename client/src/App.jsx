b0a01d5b96d09b58fd15351f172ad626cd9a443c
LinC3e <ivancostilla17@gmail.com>
2023-07-07 18:19:45 -0300

    ADD - Login page init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RegisterPage from './pages/RegisterPage'
 import { AuthProvider } from './context/AuthContext'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<h1>Home Page</h1>} />
-          <Route path='/login' element={<h1>Login Page</h1>} />
+          <Route path='/login' element={<LoginPage/>} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/tasks' element={<h1>Tasks Page</h1>} />
         </Routes>
