f710db1a1147ca6608d4e9f5b4b6d425aa8aa448
LinC3e <ivancostilla17@gmail.com>
2023-07-10 21:15:27 -0300

    ADD - Axios route login request

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,3 +3,5 @@ import axios from 'axios'
 const API = 'http://localhost:2121/api'
 
 export const registerReq = user => axios.post(`${API}/register`, user)
+
+export const loginReq = user => axios.post(`${API}/login`, user)
