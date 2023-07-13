384412f2862fda0a4f09d91ac61a3bfb57d96cba
LinC3e <ivancostilla17@gmail.com>
2023-07-13 16:16:35 -0300

    FIX - CORS config

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT
 dbConnection()
 
 //Middlewares
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cookieParser())
